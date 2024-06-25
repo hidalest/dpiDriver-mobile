fa5f4d167fa27ed062708fa86888616cfd57f9ee
Esteban Hidalgo <hidalgo.esteban2594@gmail.com>
2024-06-25 15:19:18 -0400

    feat: successfully fetch the access token

--- app/index.tsx
+++ app/index.tsx
@@ -4,31 +4,68 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import Login from '@/components/Login/Login';
 import data from '../data.json';
+import { SERVER_URL } from '@/config/apiConfig';
+import axios from 'axios';
 
 const LoginScreen = () => {
-  const {} = data.loginProps
+  const {} = data.loginProps;
 
   const navigation = useNavigation();
 
-  const handleSignIn = (username: string, password: string): void => {
+  const handleSignIn = async (
+    email: string,
+    password: string
+  ): Promise<void> => {
     // Perform login logic here
-    console.log(username)
-    console.log(password)
+    console.log(email);
+    console.log(password);
+
+    // axios
+    //   .post(SERVER_URL, { email, password })
+    //   .then(function (response) {
+    //     console.log(response);
+    //     if (!response.ok) {
+    //       throw new Error(`HTTP error! status: ${response.status}`);
+    //     }
+    //   })
+    //   .catch(function (error) {
+    //     console.error(error);
+    //   });
+
+    try {
+      const response = await fetch(SERVER_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const token = await response.json();
+      console.log(token);
+
+      // navigation.dispatch(
+      //   CommonActions.reset({
+      //     index: 0,
+      //     routes: [{ name: '(tabs)' }],
+      //   })
+      // );
+    } catch (error) {
+      console.error('Network request failed:', error);
+    }
 
     // After successful login, reset the navigation stack and navigate to the dashboard
-    navigation.dispatch(
-      CommonActions.reset({
-        index: 0,
-        routes: [{ name: '(tabs)' }],
-      })
-    );
   };
-
 
   return (
     <View style={styles.container}>
-        {/* Add login form components here */}
-        <Login onSignIn={handleSignIn} {...data.loginProps} />
+      {/* Add login form components here */}
+      <Login onSignIn={handleSignIn} {...data.loginProps} />
     </View>
   );
 };
