56359c1fe474fe4e48826c1760da4ef043f589d1
jurgen-alfaro <dev.jurgen.alfaro@gmail.com>
2024-07-10 00:27:00 -0600

    feat: Add acknowledge button props

--- components/TrainingItem/Interface.ts
+++ components/TrainingItem/Interface.ts
@@ -4,4 +4,9 @@ export interface TrainingItemProps {
     training: Training;
     descHeader: string;
     lengthText: string;
+    acknowledgedText: string;
+    acknowledgeTrainingText: string;
+    modalQuestionText: string;
+    modalConfirmText: string;
+    modalDenyText: string;
 }
