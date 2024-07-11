f1c78eb65ed24613fc1509b25b0135cd2e93785f
Esteban Hidalgo <hidalgo.esteban2594@gmail.com>
2024-07-11 13:04:01 -0400

    feat: refactor the dashboard again with creating utility functions

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,20 @@
+// src/screens/LoginScreen.tsx
+
 import { CommonActions } from '@react-navigation/native';
 import { Link, useNavigation } from 'expo-router';
 import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import Login from '@/components/Login/Login';
 import data from '../data.json';
-import {
-  API_CODE,
-  AUTH_TOKEN_URL,
-  CURRENT_USER_URL,
-  DASHBOARD_URL,
-  SERVER_URL,
-} from '@/config/apiConfig';
-import axios from 'axios';
 import { useAuth } from '@/context/authContext';
 import { usePushNotifications } from '@/hooks/usePushNotifications';
 import { getWeekNumber } from '@/utils/getWeekNumber';
+import {
+  getAuthToken,
+  getCurrentUser,
+  getDashboardData,
+} from '@/utils/apiService';
+import { API_CODE } from '@/config/apiConfig';
 
 const LoginScreen = () => {
   const {} = data.loginProps;
@@ -36,49 +36,17 @@ const LoginScreen = () => {
     try {
       setIsLoading(true);
 
-      // FETCH TO GET THE AUTH TOKEN
-      const responseToken = await fetch(AUTH_TOKEN_URL, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email,
-          password,
-        }),
-      });
-      if (!responseToken.ok) {
-        throw new Error(`HTTP error! status: ${responseToken.status}`);
-      }
-      const authToken = await responseToken.json();
-
-      // FETCH OF THE CURRENT USER
-      const responseUser = await fetch(CURRENT_USER_URL, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${authToken.access}`,
-        },
-      });
-      if (!responseUser.ok) {
-        throw new Error(`HTTP error! status: ${responseUser.status}`);
-      }
-      const dataUser = await responseUser.json();
+      const authToken = await getAuthToken(email, password);
+      const dataUser = await getCurrentUser(authToken.access);
 
       const userTransportId = dataUser.transport_id;
-      // TODO: change the search query with the userTransportId when we have the correct data and also change the current week todayWeekNumber
-      const responseDashboard = await fetch(
-        `${DASHBOARD_URL}?week=21&year=${currentYear}&search=A2OI5CH6ISLHJC`,
-        {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${authToken.access}`,
-          },
-        }
+      const dashboardData = await getDashboardData(
+        authToken.access,
+        currentYear,
+        // TODO: replace this with userTransportId and todayWeekNumber when we have actual data,
+        'A1AXYAQM887EIE', // userTransportId
+        21 //todayWeekNumber
       );
-
-      const dashboardData = await responseDashboard.json();
       const [foundUserData] = dashboardData.results;
       setUserData(foundUserData);
 
